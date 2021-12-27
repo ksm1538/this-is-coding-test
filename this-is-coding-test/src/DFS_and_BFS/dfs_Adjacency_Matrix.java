6df125020e39bb5b843d2420e9f945f0ee4870c9
sm <ksm1538@naver.com>
2021-12-27 22:51:21 +0900

    DFS_Adjacency_Matrix

---
+++ this-is-coding-test/src/DFS_and_BFS/dfs_Adjacency_Matrix.java
@@ -0,0 +1,24 @@
+package DFS_and_BFS;
+
+public class dfs_Adjacency_Matrix {
+
+	public static final int INFINITE = 999999999;
+    
+    // 2차원 배열를 이용해 인접 행렬 표현
+    public static int[][] graph = {
+        {0, 3, 9},
+        {3, 0, INFINITE},
+        {9, INFINITE, 0}
+    };
+
+    public static void main(String[] args) {
+        // 그래프 출력
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 3; j++) {
+                System.out.print(graph[i][j] + " ");
+            }
+            System.out.println();
+        }
+    }
+
+}
